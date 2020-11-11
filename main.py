4c6484f6568375113b283b5fc100c32556e7a14a
A.Prokopchenko <prokopchenko-aleksei@mail.ru>
2020-11-11 20:09:12 +0300

    осууществлена запись в файл рекордсмена с последующей перезаписью в случай выйгрыша

--- main.py
+++ main.py
@@ -1,33 +1,57 @@
 import pygame
 from pygame.draw import *
 from random import randint
+
+
 pygame.init()
 
 FPS = 2
-screen = pygame.display.set_mode((1200, 900))
+with open("WinUzers.txt", mode="r") as record_user_table:
+ record = record_user_table.read().split()
+ print('ЛУЧШИЙ ИГРОК '+record[0]+' набрал '+record[1]+'очков',sep=' ')
 
+NAME = input("введите ваше имя:")
+
+screen = pygame.display.set_mode((700, 700))
+count_win = 0
+count_lose = 0
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 GREEN = (0, 255, 0)
 MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
-BLACK = (0, 0, 0)
+INITIAL_SCREEN_COLOR = (127, 180, 240)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
 
-def click(event):
+
+
+def click():
+    """возвращает True если клик вписывается в окружность"""
     print(x, y, r)
+    """координаты круга"""
+    print(event.pos)
+    """координаты клика"""
+    x1, y2 = event.pos
+    return ((x - x1)**2 + (y - y2)**2)**0.5 <= r
+
 
 def new_ball():
+
+    '''
+    рисует новый шарик
+    '''
     global x, y, r
-    '''рисует новый шарик '''
-    x = randint(100, 1100)
-    y = randint(100, 900)
-    r = randint(10, 100)
+    x = randint(100, 600)
+    y = randint(100, 600)
+    r = randint(10, 70)
     color = COLORS[randint(0, 5)]
     circle(screen, color, (x, y), r)
 
+
+
+screen.fill(INITIAL_SCREEN_COLOR)
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
@@ -38,12 +62,26 @@ while not finished:
         if event.type == pygame.QUIT:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            click(event)
-            print('Click!')
-
+            click()
+            if click() is True:
+                count_win += 1
+                print(count_win)
+            else:
+                count_lose += 1
+        elif count_lose == 3:
+            finished = True
 
     new_ball()
+
     pygame.display.update()
-    screen.fill(BLACK)
+    screen.fill(INITIAL_SCREEN_COLOR)
+print("Вы проиграли, набрали очков - ", count_win)
+
+
+if count_win > int(record[1]):
+    print('ВЫ РЕКОРДСМЕН')
+    with open("WinUzers.txt", mode="w") as best_user_table:
+        best_user_table.write(NAME+' '+str(count_win))
+
 
 pygame.quit()
